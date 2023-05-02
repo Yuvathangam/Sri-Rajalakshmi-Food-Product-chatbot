8860e643286b35c9efcd8a7fc1b4e612e820dd45
Yuvathangam <yuvaranit19@gmail.com>
2023-05-02 19:53:22 +0530

    chatbot
    Sri Rajalakshmi Food Products

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Sri Rajalakshmi Food Product chatbot
+This project is about the chatbot for vermicelli company named Sri Rajalakshmi Food Products
  
---
+++ response.js
@@ -0,0 +1,37 @@
+const responseObj = {
+    companyname: "Sri Rajalakshmi Food Product",
+    productname: "Sri Thanalakshmi",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    goodmorning:"Good Morning! This is Sri Rajalakshmi food products (NOTE:I CAN'T ACCEPT SPACE SO TYPE WITHOUT SPACE)",
+    goodafternoon:"Good Afternoon! This is Sri Rajalshmi food products(NOTE: I CAN'T ACCEPT SPACE SO TYPE WITHOUT SPACE)",
+    owner:"S.Thangapandian",
+    hi:"Hi! This is Sri Rajalakshmi food products (NOTE:I CAN'T ACCEPT SPACE SO TYPE WITHOUT SPACE)",
+    hello:"Hi! This is Sri Rajalakshmi food products (Nakshmi food products (NOTE:I CAN'T ACCEPT SPACE SO TYPE WITHOUT SPACE)",
+    goodevening:"Good Evening! This is Sri Rajalakshmi food products (NOTE:I CAN'T ACCEPT SPACE SO TYPE WITHOUT SPACE)",
+    productavailable:"Rosted Vermicelli, Ragi Vermicelli, Payasam Vermicelli",
+    rate:"singlepiece or onebagrate ",
+    singlepiece:"Rosted Vermicelli-Rs.25,  Ragi Vermicelli-Rs.30, Payasam Vermicelli-Rs.15",
+    onebagrate:"retail or wholesale",
+    cookingdirection:"Rostedvermicelli,Ragivermicelli,Payasamvermicelli",
+    rostedvermicelli:"Fry vegetables & masala as you wish with a little oil in a pan. Add salt and 2 times of water or 400ml of water for 1 pack vermicelli. When water is boiling, add vermicelli add stir occasionally till the vermicelli is cooked. Serve hot and enjoy the taste.",
+    ragivermicelli:"Take 2 litres of plain water and required quantity of salt, put Ragi Vermicelli in it. Allow it for 3 minutes and drian the excess water. Do not keep it for more than 3 minutes.  Then cook it in the steam for 5 minutes. Now ragi vermicelli ready for your recipe. Heat adequatee quantity of oil in the frying pan, add mustard seed, black gram, onion,green chillies adequate salt etc. Then put the cooked vermicelli in thhe pan and mix thoroughly for five minutes and serve hot.",
+    payasamvermicelli:"Fry Cashewnuts and raisins in ghee and get aside. Boil the milk, cut the pack add vermicelli and simmer futher for two min. till Semiya and become soft. Add sugar and cardamom stir well till it dissolves and mix throughly.Add fried Cashew Nuts, Raisins,Saffron and ghee Semiya hot.",
+    benefit:"1)It is made up of wheat.2)It contain high protein.3)It is non sticky.4)It is the good source of fibre.5)It contain Vitamin B1,Vitamin B2 protects nerves.6)It helps in carbohydrate metavolism.",
+    contact:"9444118993 (or) 8680868188",
+    email:"srirajalakshmifoodproducts@gmail.com",
+    address:"No.43, Kamatchi Amman Nagar Extension, Nandambakkam,kundrathur,Chennai-600069.",
+    help:"Sure, I can help you. What kind of details did you want? companyname,productname,owner,benefit,rate, cookingdirection,contact,email,address,onebag,timing,startat,taste",
+    onebag:"one bag contain 20 piece",
+    timing:"9am-6pm",
+    retail:"Rosted Vermicelli-Rs.260, Ragi Vermicelli-Rs.360, Payasam Vermicelli-Rs.600",
+    wholesale:"Rosted Vermicelli-Rs.240, Ragi Vermicelli-Rs.340, Payasam Vermicelli-Rs.550",
+    startat:"The company Sri Rajalakshmi Food Products start at 1/7/2017. It has been 7 years of Sri Rajalakshmi Food Products.",
+    taste:"It taste good",
+    thanks:"No mention, This is my duty",
+    
+
+    
+
+
+};
